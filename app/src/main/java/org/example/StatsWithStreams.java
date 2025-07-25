c2d9e1adb2c87ea3a5486eccc99f2a247dfe1fc6
nunezag816 <nunezag@msoe.edu>
2025-07-25 18:44:42 -0500

    Create StatsWithStreams.java

---
+++ app/src/main/java/org/example/StatsWithStreams.java
@@ -0,0 +1,22 @@
+package example.org;
+
+import java.util.Arrays;
+
+public class StatsWithStreams {
+
+    public int maximumUsingStream(int[] nums) {
+        return Arrays.stream(nums).max().orElseThrow();
+    }
+
+    public int minimumUsingStream(int[] nums) {
+        return Arrays.stream(nums).min().orElseThrow();
+    }
+
+    public int sumUsingStream(int[] nums) {
+        return Arrays.stream(nums).sum();
+    }
+
+    public int averageUsingStream(int[] nums) {
+        return (int) Arrays.stream(nums).average().orElse(0);
+    }
+}
